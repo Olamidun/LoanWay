9abc294c6418bc9b0e8d24031baa1db8e45169f3
Olamidun <kolapoolamidun@gmail.com>
2021-09-01 01:00:40 +0100

    set up environmental variable

--- loanway/settings.py
+++ loanway/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n=dn^x8te2)u@1#-1bk(38q&cuh^cwn0cu@t7ejjngz-c7@h(i'
+# SECRET_KEY = 'django-insecure-n=dn^x8te2)u@1#-1bk(38q&cuh^cwn0cu@t7ejjngz-c7@h(i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'loanway.herokuapp.com']
 
 
 # Application definition
